a79590578f2cf8dd31ad1b6f355c6125abbfcff9
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-11 23:47:32 -0500

    WORKING

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -8,7 +8,30 @@ public class Music {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+
+    public Integer selection(Integer startIndex, String selection) {
+        Integer forward = 0;
+        Integer backward = 0;
+        Integer i = startIndex;
+        while(playList[i] != selection ) {
+            i++;
+            forward++;
+            if (i == playList.length ) {
+                i = 0;
+            }
+        }
+        i = startIndex;
+        while (playList[i] != selection) {
+            i--;
+            backward++;
+            if (i == -1) {
+                i = playList.length-1;
+            }
+        }
+        if (forward < backward) {
+            return forward;
+        } else {
+            return backward;
+        }
     }
 }
